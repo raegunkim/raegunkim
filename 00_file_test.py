ff3b9b0ccda22659f3ae7f909da9e68918511f0f
raegunkim <forjs1@gmail.com>
2023-10-16 16:16:35 +0900

    2023/10/15 first commit

---
+++ 00_file_test.py
@@ -0,0 +1,5 @@
+f = open('02_program/test.txt', 'w')
+
+
+import pickle
+f = openn('02_program/pickledat.')
---
+++ 01_namecard.py
@@ -0,0 +1,90 @@
+# 명함관리 프로그램
+# 기능(입력,수정,삭제,목록,검색,종료,저장)
+# 데이터구조(이름,주소,전화번호,이메일)- 리스트
+
+import json
+
+card = [['홍길동','세종','010-111-2222','hong@gmail.com'],
+         ['김태우','세종','010-222-2222','kim@gmail.com']]
+
+while True:
+    menu = input('''
+----------------------------------------------------------------
+    1.입력 2.수정 3.삭제 4.목록 5.검색 6.저장 7.종료
+----------------------------------------------------------------
+                 
+>>> ''')
+    if menu == '1':
+        while True:
+            name = input('이름 >>> ')
+            check = 0
+            for item in card:
+                if item[0] == name:
+                    check = 1
+            if check == 0:
+                break
+            print('중복된 이름이 있습니다.')
+
+        address = input('주소 >>> ')
+        tel = input('전화번호 >>> ')
+        email = input('이메일 >>> ')
+        card.append([name,address,tel,email])
+        print(card)
+
+    elif menu == '2':
+        name = input('수정할 이름 >>> ')
+        idx = -1
+        for item in enumerate(card):
+            if name == item[0]:
+                idx = index
+                update = int(input('수정할 항목(1.주소 2.전화번호 3.이메일)>>>'))
+                card[index][update] = input('수정내용 입력 >>> ')
+                break
+        if idx == -1:
+            print('등록된 데이터가 없습니다.')
+    elif menu == '3':
+        name = input('삭제할 이름 >>>')
+        delok = 0
+        for index, item in enumerate(card):
+            if item[0] == name:
+                print(item, '삭제~')
+                del card[index]
+                delok = 1
+                break
+        if delok == 0:
+            print('등록되지 않은 데이터입니다.')
+
+    elif menu == '4':
+        for index , item in enumerate(card):
+            print(f'''
+    {index+1}번째
+ ---------------------------------
+    이    름 : {item[0]}
+    주    소 : {item[1]}
+    전화번호 : {item[2]}     
+    이 메 일 : {item[3]}''')
+                   
+    elif menu == '5':
+        name = input('검색할 이름 >>> ')
+        idx = -1
+        for index, item in enumerate(card):
+            if item[0] == name:
+                idx = index
+                print(f'''
+    {index+1}page
+---------------------------------
+    이    름 : {item[0]}
+    주    소 : {item[1]}
+    전화번호 : {item[2]}     
+    이 메 일 : {item[3]}''')
+                break
+        if idx == -1:
+            print('등록되지 않은 데이터입니다.')
+    elif menu == '6':
+        with open('02_program/namecard.json','w') as f:
+            json.dump(card,f,indent=2)
+    elif menu == '7':
+        print('프로그램 종료!')
+        break
+    else:
+        print('메뉴선택이 잘못되었습니다.')
